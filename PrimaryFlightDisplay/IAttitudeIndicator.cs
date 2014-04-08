309013e3dc98c0dea39a0d5446a8b76324c4791d
Marcelo Delnero <marcelo.delnero@gmail.com>
2014-04-08 09:59:12 -0300

    IAttitudeIndicator and IPrimaryFlightDisplay initial commit

---
+++ PrimaryFlightDisplay/IAttitudeIndicator.cs
@@ -0,0 +1,14 @@
+﻿
+namespace PrimaryFlightDisplay
+{
+    public interface IAttitudeIndicator
+    {
+        /// <summary>
+        /// Roll Angle.</summary>
+        float RollAngle { get; set; }
+
+        /// <summary>
+        /// Pitch Angle.</summary>
+        float PitchAngle { get; set; }
+    }
+}
---
+++ PrimaryFlightDisplay/IPrimaryFlightDisplay.cs
@@ -0,0 +1,25 @@
+﻿
+namespace PrimaryFlightDisplay
+{
+    /// <summary>
+    /// Primary Flight Display Interface.
+    /// </summary>
+    public interface IPrimaryFlightDisplay
+    {
+        /// <summary>
+        /// Air Speed Tape.</summary>
+        IGauge AirSpeed { get; }
+
+        /// <summary>
+        /// Altitude Tape.</summary>
+        IGauge Altitude { get; }
+
+        /// <summary>
+        /// Compass.</summary>
+        IGauge Compass { get; }
+
+        /// <summary>
+        /// Attitude Indicator.</summary>
+        IAttitudeIndicator AttitudeIndicator { get; }
+    }
+}
